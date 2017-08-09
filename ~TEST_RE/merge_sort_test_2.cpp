06a7ae46a9246db7b573a3fb83ed070ee817e42e
pigoil <alienanimal@163.com>
2017-08-09 22:20:52 +0800

    对10000个数进行归并排序的测试

---
+++ ~TEST_RE/merge_sort_test_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include "../20170808_merge_sort/top_to_bottom_merge_sort.h"
+#include "timer_util.h"
+
+using namespace std;
+
+int main()
+{
+	const int N = 10000; 
+	int array[N];
+	string str;
+	ifstream fin;
+	fin.open("..\\~TEST_DATA\\disordered_random_double.txt", ios::in);
+	for(int i = 0;i<N;++i)
+	{
+		getline(fin,str);
+		array[i] = atoi(str.c_str());
+		//cout << atoi(str.c_str() ) << endl;
+	}
+	cout << "OK" << endl;
+	
+	Timer t;
+	MergeSort<int>::sort(array,N);
+	cout << t.elapseMsec() <<endl;	
+	
+	//for(int i = 0;i<N;++i)
+	//	cout << array[i] << endl;
+
+}
